a531af239e2a4859296749d607d1c6291d4a234d
Anon Karytski <heyheyjude23@gmail.com>
2021-12-07 20:59:00 +0300

    init

---
+++ src/text/index.tsx
@@ -0,0 +1,41 @@
+import React, { ReactNode } from 'react'
+import { Text as RawText, TextProps } from 'react-native'
+import { setDefaults, textStyles } from './styles'
+
+type ThemedTextProps = {
+  medium?: true
+  bold?: true
+  extraBold?: true
+  subline?: true
+  children?: ReactNode
+  label?: string
+} & TextProps
+
+export default function Text({
+  medium,
+  bold,
+  extraBold,
+  children,
+  style,
+  label,
+  ...props
+}: ThemedTextProps) {
+  const fontStyle = [
+    textStyles.regular,
+    textStyles.defaults,
+    medium ? textStyles.medium : null,
+    bold ? textStyles.bold : null,
+    extraBold ? textStyles.extraBold : null,
+    style,
+  ]
+
+  return (
+    <RawText style={fontStyle} {...props}>
+      {label || children}
+    </RawText>
+  )
+}
+
+Text.setDefaults = setDefaults
+
+export { textStyles }
---
+++ src/text/styles.ts
@@ -0,0 +1,91 @@
+import { BLACK, BLUE, GRAY } from '../../styles/colors'
+
+type SetDefaultFontsProps = {
+  regular?: string
+  medium?: string
+  bold?: string
+  extraBold?: string
+  size?: number
+  color?: string
+}
+
+const FONTS = {
+  REGULAR: 'IBMPlex-400',
+  MEDIUM: 'IBMPlex-500',
+  BOLD: 'IBMPlex-600',
+  EXTRA_BOLD: 'IBMPlex-700',
+}
+
+export const textStyles = {
+  defaults: {
+    color: BLACK.COMMON,
+    fontSize: 14,
+  },
+  regular: {
+    fontFamily: FONTS.REGULAR,
+  },
+  medium: {
+    fontFamily: FONTS.MEDIUM,
+  },
+  bold: {
+    fontFamily: FONTS.BOLD,
+  },
+  extraBold: {
+    fontFamily: FONTS.EXTRA_BOLD,
+  },
+  titleSmall: {
+    fontSize: 18,
+    lineHeight: 23,
+    fontFamily: FONTS.MEDIUM,
+  },
+  title: {
+    fontSize: 22,
+    textAlign: 'center',
+    fontFamily: FONTS.BOLD,
+  },
+  link: {
+    color: BLUE.COMMON,
+    fontFamily: FONTS.BOLD,
+  },
+  center: {
+    textAlign: 'center',
+  },
+  subLine: {
+    fontSize: 12,
+    fontFamily: FONTS.MEDIUM,
+    color: GRAY.COMMON,
+  },
+  font10: {
+    fontSize: 10,
+  },
+  font12: {
+    fontSize: 12,
+  },
+  font18: {
+    fontSize: 18,
+  },
+}
+
+export function setDefaults({
+  regular,
+  medium,
+  bold,
+  extraBold,
+  size,
+  color,
+}: SetDefaultFontsProps) {
+  if (regular) textStyles.regular.fontFamily = regular
+  if (medium) {
+    textStyles.medium.fontFamily = medium
+    textStyles.subLine.fontFamily = medium
+    textStyles.titleSmall.fontFamily = medium
+  }
+  if (bold) {
+    textStyles.bold.fontFamily = bold
+    textStyles.title.fontFamily = bold
+    textStyles.link.fontFamily = bold
+  }
+  if (extraBold) textStyles.extraBold.fontFamily = extraBold
+  if (size) textStyles.defaults.fontSize = size
+  if (color) textStyles.defaults.color = color
+}
