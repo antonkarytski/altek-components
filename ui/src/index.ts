c7dd68f7668a9d06930bddd0a78259f64f070e44
Anton <heyheyjude23@gmail.com>
2022-01-19 16:25:00 +0300

    0.1.9 fix exports

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -6,3 +6,5 @@ export { COLORS, Text, textStyles }
 export { shadowsStyles } from './styles/shadows'
 export { default as ExpandableCard } from './expandableCard'
 export { default as Avatar } from './avatar/Avatar'
+export { default as ImageEditor } from './cameraHandler/ImageEditor'
+export * from './cameraHandler/types'
