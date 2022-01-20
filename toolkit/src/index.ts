3101a4a11f0cf3ac5f7aa4eaf52906145b7695d5
Anton <heyheyjude23@gmail.com>
2022-01-20 16:31:42 +0300

    0.1.4 fix exports

--- toolkit/src/index.ts
+++ toolkit/src/index.ts
@@ -1,5 +1,5 @@
 export * from './hooks'
 export * from './types'
 export * from './images'
-export { createRepeatableEffect } from './effectorModels/model.repeatbleEffect'
+export * from './effectorModels/model.repeatbleEffect'
 export { createCounter } from './effectorModels/model.counter'
