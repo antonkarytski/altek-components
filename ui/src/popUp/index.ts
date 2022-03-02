27d60a8e0d74ff19f784e524179fe18b032a2fe5
Anton <heyheyjude23@gmail.com>
2022-03-02 11:28:11 +0300

    ui: 0.3.16 fix: exports name

--- ui/src/popUp/index.ts
+++ ui/src/popUp/index.ts
@@ -1,10 +1,10 @@
 export {
   createPopUpNotificationComponent,
-  default as PopUpNotification,
+  default as CommonPopUpNotification,
 } from './notification/CommonPopUpNotification'
 export {
   createPopUpSubmitComponent,
-  default as PopUpSubmit,
+  default as CommonPopUpSubmit,
 } from './submit/CommonPopUpSubmit'
 export { createPopUpManager, PopUpManager } from './model.popUpManager'
 export { createPopUpHook } from './hook'
