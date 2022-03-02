6fb74e9bf4df61e50a1b75b477bbfe202faa43f1
Anton <heyheyjude23@gmail.com>
2022-03-02 10:49:02 +0300

    ui: 0.3.14 udpate imports

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,7 +1,10 @@
 import Text, { textStyles } from './text'
 import * as COLORS from './colors'
 export { COLORS, Text, textStyles }
+export { COLOR_PRESET } from './styles/presets'
+export type { ColorPreset } from './styles/presets'
 export { shadowsStyles } from './styles/shadows'
+export { default as ColorHeader } from './colorHeader/ColorHeader'
 export { default as ExpandableCard } from './expandableCard'
 export { default as Avatar } from './avatar/Avatar'
 export { default as ImageEditor } from './cameraHandler/ImageEditor'
