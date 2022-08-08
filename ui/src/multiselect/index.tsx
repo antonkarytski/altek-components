7e34a13e615b2afb3ab6cf4489f6f4f4af4f428b
antonkarytski <heyheyjude23@gmail.com>
2022-08-08 22:20:50 +0200

    reafctor: multiselect imports

--- ui/src/multiselect/index.tsx
+++ ui/src/multiselect/index.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import RowMultiSelect from './mode.row/RowMultiSelect'
 import ModalMultiSelect from './mode.modal/ModalMultiSelect'
 import SelectMultiSelect from './mode.select/SelectMultiSelect'
-import EmptyItemsList from "./list.empty/EmptyItemsList";
-import CheckboxItemsList from "./list.checkbox/CheckboxItemsList";
-import RowItemsList from "./list.row/RowItemsList";
+import EmptyItemsList from './list.empty/EmptyItemsList'
+import CheckboxItemsList from './list.checkbox/CheckboxItemsList'
+import RowItemsList from './list.row/RowItemsList'
 
 const MultiSelect = {
   Select: SelectMultiSelect,
