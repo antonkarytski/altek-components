4e54ca728e979278e8aa47018459176a588cd5f0
antonkarytski <heyheyjude23@gmail.com>
2022-08-09 13:20:59 +0200

    0.7.15 fix: replace list with flatlist

--- ui/src/multiselect/list.row/RowItemsList.tsx
+++ ui/src/multiselect/list.row/RowItemsList.tsx
@@ -3,6 +3,7 @@ import ItemsList from '../list/ItemsList'
 import RowListItem, { rowListItemStyles } from './RowListItem'
 import { MultiSelectListProps } from '../types'
 import { composeItemStylesWithListStyles } from '../helpers'
+import { FlatList, StyleSheet } from 'react-native'
 
 export default function RowItemsList<V extends string, L extends string>({
   data = [],
@@ -17,12 +18,12 @@ export default function RowItemsList<V extends string, L extends string>({
   )
 
   return (
-    <ItemsList
-      preventScroll={inRow}
+    <FlatList
+      scrollEnabled={!inRow}
       data={data}
-      style={[style?.container, containerStyle]}
-    >
-      {({ label, value, selected }, index) => {
+      style={[styles.container, style?.container, containerStyle]}
+      contentContainerStyle={styles.containerContent}
+      renderItem={({ item: { value, selected, label }, index }) => {
         return (
           <RowListItem
             index={index}
@@ -34,6 +35,35 @@ export default function RowItemsList<V extends string, L extends string>({
           />
         )
       }}
-    </ItemsList>
+    />
   )
+  // return (
+  //   <ItemsList
+  //     preventScroll={inRow}
+  //     data={data}
+  //     style={[style?.container, containerStyle]}
+  //   >
+  //     {({ label, value, selected }, index) => {
+  //       return (
+  //         <RowListItem
+  //           index={index}
+  //           key={`${label}${value}${index}`}
+  //           onPress={onItemSelect}
+  //           label={label}
+  //           selected={selected}
+  //           style={itemStyles}
+  //         />
+  //       )
+  //     }}
+  //   </ItemsList>
+  // )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    backgroundColor: '#FFFFFF',
+    paddingVertical: 15,
+  },
+  containerContent: { flexGrow: 1 },
+})
