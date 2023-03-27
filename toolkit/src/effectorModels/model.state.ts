d5798d43b00dfcfd71a750b9694bbbc611a14a28
antonkarytski <heyheyjude23@gmail.com>
2023-03-27 11:19:54 +0200

    fix: add methods to state model

--- toolkit/src/effectorModels/model.state.ts
+++ toolkit/src/effectorModels/model.state.ts
@@ -1,12 +1,45 @@
-import { createEvent, restore, Store, Event } from 'effector'
+import {
+  createEvent,
+  restore,
+  Store,
+  Event,
+  Unit,
+  Tuple,
+  InferValueFromTupleOfUnits,
+} from 'effector'
+
+class StateModelClass<T> {
+  public readonly $state: Store<T>
+  public readonly set = createEvent<T>()
+  public readonly reset = createEvent<void | Event<any>>()
+
+  constructor(initial: T) {
+    this.$state = restore(this.set, initial).on(this.reset, (_, payload) => {
+      if (!payload) return initial
+    })
+    this.reset.watch((payload) => {
+      if (payload) this.$state.reset(payload)
+    })
+  }
+
+  on<E>(trigger: Unit<E>, reducer: (state: T, payload: E) => T | void): this
+  on<E>(triggers: Unit<E>[], reducer: (state: T, payload: E) => T | void): this
+  on<E extends Tuple<Unit<any>>>(
+    triggers: E,
+    reducer: (state: T, payload: InferValueFromTupleOfUnits<E>) => T | void
+  ): this
+  public on(triggers: any, reducer: any) {
+    this.$state.on(triggers, reducer)
+    return this
+  }
+}
 
 export function createStateModel<T>(initial: T) {
-  const set = createEvent<T>()
-  const $state = restore(set, initial)
-  return { $state, set }
+  return new StateModelClass(initial)
 }
 
 export type StateModel<T> = {
+  reset: Event<void | (() => void)>
   $state: Store<T>
   set: Event<T>
 }
